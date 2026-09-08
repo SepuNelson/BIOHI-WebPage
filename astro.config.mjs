6469ed3f9646ff854e7fea20ec19f2d35629e3a7
SepuNelson <nelson.sepulvedav@usm.cl>
2026-09-07 21:33:19 -0300

    Final

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { sites } from '@openai/sites-vite-plugin';
 
 export default defineConfig({
+  site: 'https://biohi.feriadesoftware.cl',
   vite: {
     plugins: [tailwindcss(), sites()],
   },
